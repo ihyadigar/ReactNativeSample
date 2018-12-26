9d96b583ff70c4e0ccf651b5383454439149b9b8
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2018-12-26 14:29:38 +0300

    Header component created

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component {
