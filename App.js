df1099d96cd33f7d54025b0a6d6a11dc58a436fe
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2018-12-26 14:26:23 +0300

    Initial project has been created

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+export default class App extends Component {
+  render() {
+    return (
+      //Componentin içinde 'ScrollView' varsa root view'a "flex: 1" eklenmelidir.
+      //Aksi halde tüm liste görüntülenemez(tüm ekranı kapla demektir): 
+      <View style={{ flex: 1 }}>
+        <Header headerText="Albums" />
+        <AlbumList />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
