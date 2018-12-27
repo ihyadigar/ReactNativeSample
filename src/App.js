9ab69fbbc94d99d759043d8597b8f83e933679bc
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2018-12-27 14:44:40 +0300

    Firebase package added and initialization configured.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp(
+            {
+                apiKey: 'AIzaSyAgcNRiDOc_wMlKwnTh2DIn9Nos-FFG9kk',
+                authDomain: 'reactnativeauth-a22d7.firebaseapp.com',
+                databaseURL: 'https://reactnativeauth-a22d7.firebaseio.com',
+                projectId: 'reactnativeauth-a22d7',
+                storageBucket: 'reactnativeauth-a22d7.appspot.com',
+                messagingSenderId: '452129769560'
+              }
+        );
+    }
     render() {
         return (
             <View>
