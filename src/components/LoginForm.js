80c1ffec4c0a385b7dd8a1845c46e488fef2b0c5
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2018-12-28 14:27:44 +0300

    Button press method implemented and firebase signIn and signUp logic created.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
     //React native'de TextInput yaygın bilinen yöntemden farklı çalışır(Form submit edildiğinde değeri okuma vb.)
     //TextInput değeri state içinde tutulmalı ve her karakter girişinde setState ile güncellenmelidir.
-    
-    state = { email: '', password: '' };
 
-  render() {
-    return (
-      <Card>
-          <CardSection>
-              <Input 
-                label="E-Posta:"
-                placeholder="e-posta adresinizi giriniz"
-                value={this.state.email}
-                onChangeText={email => this.setState({ email })}
-              />
-          </CardSection>
-          <CardSection>
-            <Input 
-                secureTextEntry
-                label="Şifre:"
-                placeholder="şifrenizi giriniz"
-                value={this.state.password}
-                onChangeText={password => this.setState({ password })}
-            />
-          </CardSection>
+    state = { email: '', password: '', error: '' };
 
-          <CardSection>
-              <Button>
-                  Giriş
+    onButtonPress() {
+        const { email, password } = this.state;
+        
+        //Kullanıcının girdiği mail ve şifre ile firebase'in signIn metodu kullanılarak giriş yapılır
+        //Eğer giriş başarısız olursa catch() metoduna düşülür ve bu adımda yeni hesap oluşturmaya yönlendirir.
+        //Eğer hesap oluşturma da başarısız olursa ekranda gösterilecek error state'i
+        //komponentin rerender olması için güncellenir.
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(() => {
+                this.setState({ error: 'Giriş Başarısız Oldu.' });
+            });
+        });
+
+        //Yeni giriş için hata mesajını temizle
+        this.setState({ error: '' });
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label="E-Posta:"
+                        placeholder="e-posta adresinizi giriniz"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Şifre:"
+                        placeholder="şifrenizi giriniz"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
+
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
+                <CardSection>
+                    <Button onButtonPress={this.onButtonPress.bind(this)}>
+                        Giriş
               </Button>
-          </CardSection>
-      </Card>
-    );
-  }
+                </CardSection>
+            </Card>
+        );
+    }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
 
 export default LoginForm;
