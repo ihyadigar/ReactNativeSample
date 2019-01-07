d9b80cfbb0d6e6a47b9daa1c29262160fc89987c
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2019-01-07 15:53:36 +0300

    Individual library item handler is added.

---
+++ src/components/LibraryListItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+export default class LibraryListItem extends Component {
+    render() {
+        const { titleStyle } = styles;
+
+        return (
+            <CardSection>
+                <Text style={titleStyle}>
+                    {this.props.library.item.title}
+                </Text>
+            </CardSection>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
