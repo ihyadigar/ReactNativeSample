de7c5203efeb93db6b6aa16b7eb471fc8fe69a01
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2019-01-14 10:30:27 +0300

    Firebase api keys moved to the private config file.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from '@firebase/app';
 import '@firebase/auth';
+import firebaseConfig from './utils/config';
 import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
@@ -10,16 +11,7 @@ class App extends Component {
     state = { loggedIn: null };
 
     componentWillMount() {
-        firebase.initializeApp(
-            {
-                apiKey: 'AIzaSyAgcNRiDOc_wMlKwnTh2DIn9Nos-FFG9kk',
-                authDomain: 'reactnativeauth-a22d7.firebaseapp.com',
-                databaseURL: 'https://reactnativeauth-a22d7.firebaseio.com',
-                projectId: 'reactnativeauth-a22d7',
-                storageBucket: 'reactnativeauth-a22d7.appspot.com',
-                messagingSenderId: '452129769560'
-            }
-        );
+        firebase.initializeApp(firebaseConfig);
 
         //Eğer giriş yapılmışsa bir "user" nesnesi oluşacak, giriş yapılmamışsa user nesnesi oluşmayacak:
         firebase.auth().onAuthStateChanged((user) => {
